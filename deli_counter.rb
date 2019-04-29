23565add9d26becb792180fffeff3bd7d3a3f180
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-29 22:18:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def katz_deli(line)
   line = []
   counter = 0
   while counter == line.length[0]
-  line = "The line is currently empty." 
+  line = "The line is empty." 
   counter +=1
   end
 end
